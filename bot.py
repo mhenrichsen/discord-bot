bf4e84196dfab9c6142b715910c6302659a94cab
mhenrichsen <mads.gade.henrichsen@live.dk>
2023-01-08 22:31:26 +0100

    updated

--- bot.py
+++ bot.py
@@ -10,13 +10,14 @@ from time import sleep
 
 def write_message(url: str, message: str):
     # open browser headless
-    browser = webdriver.Chrome('/Users/mads/Desktop/discord-bot/chromedriver')
-    browser.get('https://discord.com/channels/840898407022460948/844470172001894400')
+    browser = webdriver.Chrome('chromedriver.exe')
+    browser.get('https://discord.com/channels/611132910992490506/611135278454669332')
+    browser.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[1]/div/div/div/section/div[2]/button[2]/div').click()
     browser.find_element(By.NAME, "email").send_keys("m@fpvgear.dk")
-    browser.find_element(By.NAME, "password").send_keys("********")
+    browser.find_element(By.NAME, "password").send_keys("*******")
     browser.find_element(By.NAME, "password").send_keys(Keys.RETURN)
     sleep(15)
-    chat = '//*[@id="app-mount"]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[3]/div[2]/main/form/div/div/div/div[1]/div/div[3]/div/div[2]'
+    chat = '//*[@id="app-mount"]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[2]/div[2]/main/form/div[1]/div[1]/div/div[3]/div/div[2]'
     chat = browser.find_element(By.XPATH, chat)  
     # split string on newline
     message_split = message.splitlines()
--- scheduler.py
+++ scheduler.py
@@ -2,40 +2,41 @@ import schedule
 import time
 from bot import write_message
 
-schedule.every().minute.do(write_message, "https://discord.com/channels/840898407022460948/844470172001894400", """**<Animal Prison> is recruiting! INTERESTED IN PLAYERS FOR WOTLK** 
+schedule.every(6).hours.do(write_message, "https://discord.com/channels/611132910992490506/611135278454669332", """<hov> is a Danish guild recruiting to bolster our raiding roster. 
+Naxx-25 sub 1 hour
+Immortal runs!
+https://classic.warcraftlogs.com/reports/L1vDXYZFrqByK34x
 
-Here is why you should join us: 
-```diff
-+ Currently progessing in SWP (lots of fun!)
-+ Hyjal 5/5, BT 9/9 - First week
-+ Gruul/Kara cleared first week, Mag second!
-+ SSC 6/6, TK 4/4 (pre-nerf ofc). Clearing all in 1 night + Gruul/Mag
-+ We got Bob Ross. Shit hunter but good painter
-+ We have more fun
-+ We have our own special wheelchair system. Do something retarded in raid and get a wheelchair appended to your Discord name! [Patent Pending]
-+ Transparent and easy to understand loot system
-```
-We need you (if you are not mentally challenged and play one of the following classes): 
-```fix
-∞x ANY PLAYER/CLASS FOR WOTLK
-1x Warlock
-1x Resto shaman
-1x Resto druid
-1x Boomkin
-1x Shadow Priest
-1x Arms Warrior
-∞x Good players of any class feel free to reach out
-```
-Raid times:
-```diff
-- Wednesday 19:30-23
-- Sunday 19:30-23
-```
-We do our best to have as much fun as possible while clearing all content fast and efficient. We expect our raiders to come with all consums and overall prepared for our raids.
+<hov> søger friske raiders!
+Om os:
+Vi er en dansk-talende guild, hvori mange af os har spillet sammen siden starten af classic. Vi er et semi-hardcore guild og regner med at enchants og consumes er i orden på både main og mindst én alt. Vi stræber efter en hyggelig raid-atmosfære, hvor det er muligt at holde fokus når der skal laves progression. Vi har en Discord hvor der ofte sidder folk og hygger, og der er altid nogen klar til at hjælpe med en heroic eller to.
 
-Please join our discord or reach out to me or @Knagg  for more information! https://discord.gg/aNVNgtzBGp
+https://classic.warcraftlogs.com/guild/eu/gehennas/hov
+
+Hvad vi forventer fra vores spillere:
+- At du er mødestabil
+- At du har kendskab til din class, og kan tage imod konstruktiv kritik
+- At enchants og consumes er i orden på både main og alt
+- At du er forberedt på nyt content, og har læst op på strats på forhånd
+
+Loot: Loot-council
+
+Vi rekrutterer:
+- Warlocks
+- Rogues
+- Death Knights
+- Andre gode spillere
+
+Derudover tager vi selvfølgelig altid imod dygtige spillere af alle classes, og socials
+
+Raid-skema:
+Onsdag 19:00 - 23:00
+Søndag 19:00 - 23:00
+
+Smid @dresdres en besked på discord, eller hop ind på vores discord og tag fat i vores Hovding eller en Hovfficer for en snak.
+https://discord.gg/p22cZ7XVsW
 """)
 
 while True:
     schedule.run_pending()
-    time.sleep(60)
+    time.sleep(5)
